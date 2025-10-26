3d6adee2719ee54216417bdb4311801556e3922d
Kyle Chen <78221899+K-L-Chen@users.noreply.github.com>
2025-10-26 21:45:41 +0000

    Update treadmill.py

--- treadmill.py
+++ treadmill.py
@@ -1,25 +1,34 @@
 import time
 from pynput.keyboard import Key, Listener
-from pynput.mouse import Controller
+from pynput.mouse import Controller, Button, Listener as mouse_listener
 from PyQt6 import QtCore
 from PyQt6.QtWidgets import QApplication, QWidget, QPushButton, QVBoxLayout, QLineEdit, QLabel
 import vgamepad as vg
+from math import cos, sin
+import openvr
+import ctypes
 
 gamepad = vg.VX360Gamepad()
 mouse = Controller()
 enabled = True
 keyToggle = False
-
+clickToggle = False
+clickStop = False
 # -------------------------------------------------------------------
-sensitivity = 150 # How sensitive the joystick will be
+sensitivity = 75 # How sensitive the joystick will be
 pollRate = 30 # How many times per second the mouse will be checked
 quitKey = Key.ctrl_r # Which key will stop the program
+mouseX = 700 #reset x position for mouse
+mouseY = 500 #reset y position for mouse
+base_hmd_pose = None
 # -------------------------------------------------------------------
 
 class MainWindow(QWidget):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+
+        global base_hmd_pose
 
         self.setWindowTitle("Maratron")
         
@@ -53,6 +62,11 @@ class MainWindow(QWidget):
         
         self.setLayout(layout)
         self.show()
+        #vr_system = openvr.init(openvr.VRApplication_Scene)
+        openvr.init(openvr.VRApplication_Overlay)
+        _, _, devposes = openvr.VRSystem().getControllerStateWithPose(1,openvr.k_unTrackedDeviceIndex_Hmd)
+        base_hmd_pose = devposes.mDeviceToAbsoluteTracking
+
         
     def setPollingRate(a, b):
         global pollRate
@@ -80,10 +94,48 @@ class MainWindow(QWidget):
             print("Confirmed")
             keyToggle = False
         
-        
     def run(a, b):
         global enabled
         global keyToggle
+        global mouseX
+        global mouseY
+
+        #Rotate the forward movement vector by the gyro status of the headset using pyopenvr
+        #This way, we can go and look around without having the legs follow where we're facing.
+        #TODO implement a way to stop this function
+        #@return tuple, x_value and y_value
+        def rotate(mousey:int):
+            global vr_system
+            global base_hmd_pose
+            global clickToggle
+
+            if not clickToggle:
+                #TODO calculate theta from HMD reading, last position
+                _, _, devposes = openvr.VRSystem().getControllerStateWithPose(1,openvr.k_unTrackedDeviceIndex_Hmd)
+                if base_hmd_pose != None:
+                    hmd_pose = devposes.mDeviceToAbsoluteTracking
+                    hmd_ang_vel = devposes.vAngularVelocity[0] #we only care about X rotation
+                    #if the change is not significant from a physical distance standpoint,
+                    #we don't care either
+                    if abs(hmd_pose[0][3] - base_hmd_pose[0][3]) > 0.1:
+                        #calculate theta from the angular rate of change * time elaped since
+                        #last poll (i.e. 1 / pollRate)
+                        #only calc if change is significant (> 1 deg, ~ 0.0175 rad)
+                        change = hmd_ang_vel * (1 / pollRate)
+                        if (change * change > 0.0003):
+                            theta += change
+                else:
+                    theta = 0
+
+                cost = cos(theta)
+                sint = sin(theta)
+                if theta < 0:
+                    return (int(mousey * sint), int(mousey * cost))
+                else:
+                    return (int((-1) * mousey * sint), int(mousey * cost))
+            else:
+                return (0, mousey)
+
         enabled = True
         mousey = 0
         mousey1 = 0
@@ -92,13 +144,14 @@ class MainWindow(QWidget):
             mousey2 = mousey1
             mousey1 = 0
             
-            mousey1 = (mouse.position[1] - 500) * -(sensitivity) # convert mouse position to joystick value
+            mousey1 = (mouse.position[1] - mouseY) * -(sensitivity) # convert mouse position to joystick value
             
             mousey = max(-32768, min(32767, int((mousey1 + mousey2)/2))) # average and clamp
-            mouse.position = (700, 500) # reset mouse position, CHANGE THIS IF THE MOUSE IS OFF SCREEN
+            mouse.position = (mouseX, mouseY)
             print("Joystick y:", mousey)
             
-            gamepad.left_joystick(x_value=0, y_value=mousey)  # values between -32768 and 32767
+            xval, yval = rotate(mousey)
+            gamepad.left_joystick(xval, yval)  # values between -32768 and 32767
             
             gamepad.update()
             
@@ -108,18 +161,36 @@ def onPress(key):
     global enabled
     global keyToggle
     global quitKey
+
     if keyToggle:
         print("Stop key will be", str(key))
         quitKey = key
     elif key == quitKey:
-        enabled = False
+        enabled = False 
         print("Stopped with", quitKey)
-        
-listener = Listener(onPress)
-listener.start()
-        
-app = QApplication([])
-window = MainWindow()
-window.show()
-app.exec()
 
+def onClick(x, y, button, pressed):
+    global clickToggle
+    global clickStop
+    
+    if button == Button.left:
+        clickToggle = True
+
+    elif button == Button.right:
+        clickStop = True
+        return False
+
+def main():            
+    listener = Listener(onPress)
+    mlistener = mouse_listener(on_click=onClick)
+    listener.start()
+    mlistener.start()
+            
+    app = QApplication([])
+    window = MainWindow()
+    window.show()
+    app.exec()
+    return
+
+if __name__ == "__main__":
+    main()
